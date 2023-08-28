658e803e396462ee238aafb7ee353a500113e958
Nestor Valles <nestor@vlue.company>
2023-08-28 14:42:00 -0500

    whiteSquare added

---
+++ src/app/whiteSquare.js
@@ -0,0 +1,7 @@
+function whiteSquare () {
+  const whiteSquare = document.createElement('div');
+  whiteSquare.classList.add('white__square');
+  document.body.appendChild(whiteSquare);
+}
+
+export default whiteSquare;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import carouselAnimation from './app/carousel';
 import toggleNav from './app/nav';
 import showContent from './app/impact';
 import addStylesheet from './app/styles';
-
+import whiteSquare from './app/whiteSquare';
 
 document.addEventListener('DOMContentLoaded', function() {
     addStylesheet();
+    whiteSquare();
     toggleNav();
     const page = window.location.pathname;
   
