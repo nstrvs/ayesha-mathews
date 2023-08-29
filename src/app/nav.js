63283d4a892891ec9755c596fae27333f3a473a9
Nestor Valles <nestor@vlue.company>
2023-08-29 14:43:02 -0500

    nav debugging

--- src/app/nav.js
+++ src/app/nav.js
@@ -11,24 +11,26 @@ function addNav() {
       <li><a class="nav__item" href="/terrain">Terrain</a></li>
       <li><a class="nav__item" href="/impact">Impact</a></li>
   </ul>
-  <div class="nav__icons-wrapper">
-  <a class="nav__icon-footer" href="https://www.linkedin.com/in/ayeshamathews/" target="inline"><img src='https://uploads-ssl.webflow.com/647b560fa041413036759f33/64ee4206393eb5afde4a8263_twitter.svg'></img></a>
-  <a class="nav__icon-footer" href="" target"inline"><img src='https://uploads-ssl.webflow.com/647b560fa041413036759f33/64ee420a1b07b9e11d8126bf_linkedin.svg'></img></a>
+  <div id="nav-footer"class="nav__footer closed">
+  <a class="nav__footer-item" href="https://www.linkedin.com/in/ayeshamathews/" target="inline"><img src='https://uploads-ssl.webflow.com/647b560fa041413036759f33/64ee4206393eb5afde4a8263_twitter.svg'></img></a>
+  <a class="nav__footer-item" href="" target"inline"><img src='https://uploads-ssl.webflow.com/647b560fa041413036759f33/64ee420a1b07b9e11d8126bf_linkedin.svg'></img></a>
   </div>
   `;
   document.body.insertBefore(nav, document.body.firstChild);
 }
 
 function toggleNav() {
-   const nav = document.getElementById('nav');
+    const nav = document.getElementById('nav');
     const navOpen = document.getElementById('nav-open');
     const navClose = document.getElementById('nav-close');
     const navLinks = document.getElementById('nav-links');
+    const navFooter = document.getElementById('nav-footer');
 
     navOpen.addEventListener('click', () => {
         navOpen.classList.add('closed');
         navClose.classList.remove('closed');
         navLinks.classList.remove('closed');
+        navFooter.classList.remove('closed');
         nav.style.backgroundColor = '#fff';
     });
 
@@ -36,6 +38,7 @@ function toggleNav() {
         navOpen.classList.remove('closed');
         navClose.classList.add('closed');
         navLinks.classList.add('closed');
+        navFooter.classList.add('closed');
         nav.style.backgroundColor = 'transparent';
     });
 }
