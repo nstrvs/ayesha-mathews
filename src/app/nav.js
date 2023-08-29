75508d60ae9d34804fa6ad61356649e6202ce0a1
Nestor Valles <nestor@vlue.company>
2023-08-29 17:26:09 -0500

    nav debugging

--- src/app/nav.js
+++ src/app/nav.js
@@ -39,6 +39,7 @@ function toggleNav() {
 
         if (window.innerWidth <= 479) {
             nav.style.height = '100vh';
+            body.style.overflow = 'hidden';
         }
     });
 
@@ -51,6 +52,7 @@ function toggleNav() {
 
         if (window.innerWidth <= 479) {
             nav.style.height = '10vh';
+            body.style.overflow = 'visible';
         }
     });
 }
